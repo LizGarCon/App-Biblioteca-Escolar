79c17ea78557bff9e3b930289d8df3b3dc86bfa0
LizGarCon <89824155+LizGarCon@users.noreply.github.com>
2022-06-30 22:10:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVJr_ggtDFutCGXdsvwveVBjd1WcUYSis",
-  authDomain: "db-c71-c72.firebaseapp.com",
-  projectId: "db-c71-c72",
-  storageBucket: "db-c71-c72.appspot.com",
-  messagingSenderId: "889947927729",
-  appId: "1:889947927729:web:c0608cd6a8725aa6eede90",
-  measurementId: "G-ZE4NJEF6PS"
+
 };
 
 firebase.initializeApp(firebaseConfig);
